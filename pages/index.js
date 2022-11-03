7027004d6f8059d4736adb7caf5395523b460ab4
saw <saw>
2022-11-03 14:30:00 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Test from "../pages/test";
 
 
 export default function Home() {
     return (
         <div>
             <a className="text-color-red underline text-[#50d71e]">Test</a>
+            < Test />
         </div>
     );
 }
